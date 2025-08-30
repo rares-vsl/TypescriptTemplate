20a74ef519ec4c01b440c7e8f5939b662e4b7826
Vasiliu Rares <rv.vasiliu@gmail.com>
2025-08-30 15:26:29 +0200

    build: first build
    Signed-off-by: Vasiliu Rares <rv.vasiliu@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.28"
 }
 
 gitHooks {
-    commitMsg { conventionalCommits() }
-    createHooks()
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
 }
 
 rootProject.name = "TypescriptTemplate"
